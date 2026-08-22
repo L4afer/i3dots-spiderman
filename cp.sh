b166db3ab08001e9274b4ec7a48828c55bb778fa
L4afer <leaferfr@gmail.com>
2026-08-22 04:38:13 +0200

    Update 5 - rofi redesign

--- cp.sh
+++ cp.sh
@@ -7,7 +7,3 @@ cp -r ~/Downloads/i3dots-spiderman/.config/neofetch ~/.config
 cp -r ~/Downloads/i3dots-spiderman/.config/kitty ~/.config/
 cp -r ~/Downloads/i3dots-spiderman/.config/kitty/.zshrc ~/
 cp -r ~/Downloads/i3dots-spiderman/.config/i3 ~/.config/
-sudo chmod +x ~/.config/i3/scrot.sh
-sudo chmod +X ~/.config/i3/scrot1.sh
-sudo chmod +x ~/.config/i3/kblayout.sh
-sudo chmod +x ~/.config/i3/polr.sh
