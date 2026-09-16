8fe51ec66679114d00e7054893d6ce4cf94b36fd
L4afer <leaferfr@gmail.com>
2026-09-16 17:03:47 +0200

    cp.sh update

--- cp.sh
+++ cp.sh
@@ -7,4 +7,6 @@ cp -r ~/Downloads/i3dots-spiderman/.config/neofetch ~/.config
 cp -r ~/Downloads/i3dots-spiderman/.config/kitty ~/.config/
 cp -r ~/Downloads/i3dots-spiderman/.config/kitty/.zshrc ~/
 cp -r ~/Downloads/i3dots-spiderman/.config/i3 ~/.config/
+i3 reload
+~/.config/i3/polr.sh
 betterlockscreen -u ~/.config/i3/wallhaven-7p35lv_1366x768.png
